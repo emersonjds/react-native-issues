d8fdf6ee0f7a4a06cac477ab162b227f5e67eb7e
Emerson Silva <emerson_jdss@hotmail.com>
2019-03-19 20:10:48 -0300

    config home styles

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,7 +2,40 @@ import { StyleSheet } from "react-native";
 import { colors, metrics } from "~/styles";
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    backgroundColor: colors.lighter,
+    flex: 1
+  },
+  empty: {
+    color: colors.dark,
+    marginTop: metrics.baseMargin * 2,
+    textAlign: "center"
+  },
+  error: {
+    color: colors.danger,
+    fontSize: 12,
+    fontWeight: "bold",
+    marginTop: metrics.basePadding,
+    textAlign: "center"
+  },
+  form: {
+    color: colors.dark
+  },
+  formInput: {
+    backgroundColor: colors.white,
+    borderRadius: metrics.baseRadius,
+    color: colors.darker,
+    flex: 1,
+    fontSize: 14,
+    marginRight: metrics.baseMargin,
+    padding: metrics.basePadding / 2
+  },
+  inputContainer: {
+    padding: metrics.basePadding
+  },
+  placeholderColor: {
+    color: colors.regular
+  }
 });
 
 export default styles;
